7a9f212ed21f71e46d87c30c10df57170f9e956f
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-03-09 13:45:24 +0800

    Add mac calendar

---
+++ src/actions/macCalendarAndAddress.js
@@ -0,0 +1,26 @@
+const Action = require('../action');
+const { resolvePaths } = require('../util/system');
+const { testAndGuideRestrictedDir } = require('../helpers/file');
+
+class Keychain extends Action {
+	preClean(helpers) {
+		const paths = resolvePaths([':home/Library/Calendar', ':home/Library/Application Support/AddressBook/']);
+		testAndGuideRestrictedDir(paths[0], helpers);
+		testAndGuideRestrictedDir(paths[1], helpers);
+	}
+	selectFiles() {
+		return {
+			toSave: [
+				':home/Library/Application Support/AddressBook/*.aclcddb*',
+				':home/Library/Application Support/AddressBook/*.abcddb*',
+				':home/Library/Application Support/AddressBook/Sources/*/*.abcddb*',
+
+				':home/Library/Calendar/*.caldav',
+				':home/Library/Calendar/*.calendar',
+				':home/Library/Calendar/*.plist',
+			]
+		}
+	}
+}
+
+module.exports = Keychain;
