ba8575a250021be45c936201f734e732ab6b4d6f
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-03-09 13:46:37 +0800

    Add check for restricted folder

--- src/actions/macKeychain.js
+++ src/actions/macKeychain.js
@@ -1,6 +1,12 @@
 const Action = require('../action');
+const { resolvePaths } = require('../util/system');
+const { testAndGuideRestrictedDir } = require('../helpers/file');
 
 class Keychain extends Action {
+	preClean(helpers) {
+		const path = resolvePaths([':home/Library/Keychains']);
+		testAndGuideRestrictedDir(path[0], helpers);
+	}
 	selectFiles() {
 		return {
 			toSave: [
