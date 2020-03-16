b4a3140a894e9f0e3a4141085e9acef1aa34b87e
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-03-17 07:02:46 +1300

    Use editor prompt to collect instructions

--- src/actions/instructions.js
+++ src/actions/instructions.js
@@ -1,14 +1,26 @@
 const Action = require('../action');
 
+const methods = ['beforeClean', 'afterClean', 'beforeRestore', 'afterRestore'];
+
 class Instructions extends Action {
 	constructor() {
 		super();
-		const methods = ['beforeClean', 'afterClean', 'beforeRestore', 'afterRestore'];
 		methods
 			.map(m => `${m}Notes`)
 			.forEach(method => {
 				this.method = () => this.config[method];
 			});
+	}
+
+	async interview(helpers) {
+		const { inquirer } = helpers;
+		const config = await inquirer
+			.prompt(methods.map(name => ({
+					type: 'editor',
+					name,
+					message: `Instructions to display ${startCase(name).toLowerCase()}`,
+				})));
+		return config;
 	}
 }
 
